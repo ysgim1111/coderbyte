c450668187695bbf1cd64dc4fab78fc3488a8a74
ysgim1111 <anjella6636@gmail.com>
2015-07-28 12:19:52 +0900

    Create reverse.rb

---
+++ reverse.rb
@@ -0,0 +1,12 @@
+def FirstReverse(str)
+
+  # code goes here
+  str = str.reverse
+
+  return str 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstReverse(STDIN.gets)           
